50c810df5697e4eb876571ef131ae3c45d09cf8d
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-03-13 15:35:25 +0900

    Add scripts

---
+++ send_ngrok_http_url_to_slack.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ $# == 0 ]; then 
+    echo "Usage: $0 SLACK_WEBHOOK_URL"
+    exit 1
+fi
+
+HOSTNAME=`hostname`
+NGROK_URL=`curl -s localhost:4040/api/tunnels | jq -r .tunnels[].public_url | grep --color=never http://*`
+SLACK_WEBHOOK_URL=$1
+
+if [ "$NGROK_URL" != "" ]; then
+    TEXT="[$HOSTNAME]\n$NGROK_URL"
+    curl -s -X POST -H 'Content-type: application/json' --data '{"text":"'"$TEXT"'"}' $SLACK_WEBHOOK_URL > /dev/null
+    PREV_NGROK_URL=$NGROK_URL
+else
+    PREV_NGROK_URL=""
+fi
+
+while :
+do
+    NGROK_URL=`curl -s localhost:4040/api/tunnels | jq -r .tunnels[].public_url | grep --color=never http://*`
+    if [ "$NGROK_URL" != "$PREV_NGROK_URL" ]; then
+        if [ "$NGROK_URL" = "" ]; then
+            TEXT="[$HOSTNAME] connecting"
+            curl -s -X POST -H 'Content-type: application/json' --data '{"text":"'"$TEXT"'"}' $SLACK_WEBHOOK_URL > /dev/null
+        else
+            if [ "PREV_NGROK_URL" != ""]; then
+                TEXT="[$HOSTNAME] disconnect"
+                curl -s -X POST -H 'Content-type: application/json' --data '{"text":"'"$TEXT"'"}' $SLACK_WEBHOOK_URL > /dev/null
+            fi
+        fi
+
+        while [ "$NGROK_URL" = "" ]
+        do
+            NGROK_URL=`curl -s localhost:4040/api/tunnels | jq -r .tunnels[].public_url | grep --color=never http://*`
+            sleep 1
+        done
+
+        TEXT="[$HOSTNAME]\n$NGROK_URL"
+        curl -s -X POST -H 'Content-type: application/json' --data '{"text":"'"$TEXT"'"}' $SLACK_WEBHOOK_URL > /dev/null
+        PREV_NGROK_URL=$NGROK_URL
+    fi
+
+    sleep 1
+
+done
+
+exit 0
+
